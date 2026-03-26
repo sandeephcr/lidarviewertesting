b709e824c93662ce32598b5fe3d9ab998a70b7ee
Test-Atchyutha <atchyutha.padarthi@hcrobo.com>
2026-03-26 14:40:21 +0530

    Added 3 cases and deleted unnecessary files

--- playwrighttests/tests/example.spec.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test('homepage has title and links to intro page', async ({ page }) => {
-  await page.goto('https://playwright.dev/')
-  await expect(page).toHaveTitle(/Playwright/)
-  await page.getByRole('link', { name: 'Get started' }).click()
-  await expect(page).toHaveURL(/.*intro/)
-})
